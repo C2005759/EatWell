0df860c7cb73b5a91468d5fff403944fd3c675ca
Shilin Li <c2005759@newcastle.ac.uk>
2024-07-28 22:23:51 +0100

    Add main view, ContentView, DailyPlanView, and ShoppingListView (sample implementation)
    - Implemented basic structures for main view, ContentView, DailyPlanView, and ShoppingListView.
    - Note: These views are sample implementations and still require further construction and refinement.

---
+++ EatWell_V2/Views/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  EatWell_V2
+//
+//  Created by Shilin Li on 28/07/2024.
+//
+
+import Foundation
---
+++ EatWell_V2/Views/ShoppingListView.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingListView.swift
+//  EatWell_V2
+//
+//  Created by Shilin Li on 28/07/2024.
+//
+
+import Foundation
